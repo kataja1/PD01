544ef9ff11f93858dd19aae38dc0d58c36a940a5
kataja1 <krissgutmane@gmail.com>
2024-11-08 11:52:15 +0200

    Add files via upload

---
+++ PD01.py
@@ -0,0 +1,14 @@
+try:
+    x = int(input("x: "))
+    y = int(input("y: "))
+    result1 = 4 * x ** (y + 3 ) + 15 * y
+    print (result1)
+    
+    x1 = int(input("x1: "))
+    y1 = int(input("y1: "))
+    result2 = 5 * y ** x - 144 * x + 2 / (x + y) ** 2
+    print (result2)
+    
+    x2 = int(input("x2: "))
+    y2 = int(input("y2: "))
+    result3 = 2 + x - 2 * x * y / y / x + y
